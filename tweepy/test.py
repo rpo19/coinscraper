53c6bd2d5748997e25f665f32eb0f224b7c9ae26
rpo19 <pozzir00@gmail.com>
2020-07-28 15:21:15 +0200

    spark tests

--- tweepy/test.py
+++ tweepy/test.py
@@ -1,5 +1,9 @@
 import sys
 import tweepy
+import kafka
+import json
+
+kafka_servers = ['localhost:9092']
 
 consumer_key = None
 consumer_secret = None
@@ -13,10 +17,15 @@ exec(sys.stdin.read())
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
+producer = kafka.KafkaProducer(bootstrap_servers=kafka_servers,
+                         value_serializer=lambda x: 
+                         json.dumps(x).encode('utf-8'))
+
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         print(status.text)
+        producer.send('test', value=status.text)
 
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = auth, listener=myStreamListener)
