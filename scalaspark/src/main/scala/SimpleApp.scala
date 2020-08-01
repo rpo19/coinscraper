37fda1dfa27d1f4502c4ce8bbbcdd0fd904ea2bc
rpo19 <pozzir00@gmail.com>
2020-08-01 13:45:59 +0200

    made shell working

--- scalaspark/src/main/scala/SimpleApp.scala
+++ scalaspark/src/main/scala/SimpleApp.scala
@@ -5,10 +5,21 @@ object Main {
 
     val spark = SparkSession.builder().appName("Cons test").getOrCreate()
 
-    val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "test").load()
+    val df = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "test")
+      .load()
+    val df2 = df
+      .selectExpr("CAST(value AS STRING)")
 
     //df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").as[(String, String)]
-    df.writeStream.start()
+    //https://www.thetopsites.net/article/51617823.shtml
+    val query = df2.writeStream
+      .format("console")
+      .option("truncate", "false")
+      .start()
+      .awaitTermination()
 
   }
 
