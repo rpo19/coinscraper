1ddfe312d51feedf004a50d5bf14a5bccccde149
rpo19 <pozzir00@gmail.com>
2020-08-04 18:29:18 +0200

    tweets schema

--- scalaspark/src/main/scala/SimpleApp.scala
+++ scalaspark/src/main/scala/SimpleApp.scala
@@ -1,9 +1,41 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.Trigger
 import scala.concurrent.duration._
+import org.apache.spark.sql.types.{StructType, DateType, TimestampType}
+import org.apache.spark.sql.functions.from_json
 
 object Main {
   def main(args: Array[String]) {
+
+    val tweets_schema = new StructType()
+    .add("created_at", DateType)
+    .add("id", "int")
+    .add("id_str", "string")
+    .add("text", "string")
+    .add("source", "string")
+    .add("truncated", "boolean")
+    .add("in_reply_to_status_id", "string")
+    .add("in_reply_to_status_id_str", "string")
+    .add("in_reply_to_user_id", "string")
+    .add("in_reply_to_user_id_str", "string")
+    .add("in_reply_to_screen_name", "string")
+    .add("user", "string")
+    .add("geo", "string")
+    .add("coordinates", "string")
+    .add("place", "string")
+    .add("contributors", "string")
+    .add("is_quote_status", "string")
+    .add("quote_count", "int")
+    .add("reply_count", "int")
+    .add("retweet_count", "int")
+    .add("favorite_count", "int")
+    .add("entities", "string")
+    .add("favorited", "string")
+    .add("retweeted", "string")
+    .add("possibly_sensitive", "string")
+    .add("filter_level", "string")
+    .add("lang", "string")
+    .add("timestamp_ms", TimestampType)
 
     val spark = SparkSession.builder().appName("Cons test").getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
@@ -15,20 +47,15 @@ object Main {
     val tweets = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "test")
+      .option("subscribe", "tweets-bitcoin")
       .load
-      .selectExpr("cast (value as string) value")
-
-    val words = tweets.as[String].flatMap(_.split(" "))
-    val wordCounts = words.groupBy("value").count().orderBy($"count".desc)
-
-    val query = wordCounts.writeStream
-      .outputMode("complete")
+      .select(from_json($"value".cast("string"), tweets_schema).alias("value"))
+      .writeStream
       .format("console")
       .start()
 
-    query.awaitTermination
-    query.stop
+    tweets.awaitTermination
+    tweets.stop
 
     // val df2 = df
     //   .selectExpr("CAST(value AS STRING)")
