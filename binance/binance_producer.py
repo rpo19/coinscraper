e13c2d55b7d1959e1d990cc557b14a33f9880185
rpo19 <pozzir00@gmail.com>
2020-09-01 22:46:29 +0200

    improved producers: added cli options

--- binance/binance_producer.py
+++ binance/binance_producer.py
@@ -4,23 +4,34 @@ import kafka
 import json
 import secrets
 import time
+import click
 
-kafka_servers = ['localhost:9092']
+@click.command()
+@click.option('-v', '--verbose', default=False, is_flag=True, help='Verbose mode')
+@click.option('-s', '--symbol', help='Symbol to monitor', default='BTCUSDT')
+@click.option('-k', '--kafka', 'kafka_servers', multiple=True, help='Kafka server. You can add more than one', default=["localhost:9092"])
+@click.option('-t', '--topic', 'kafka_topic', help='Kafka topic in which to publish', default='binance-BTCUSDT')
+def go(verbose, symbol, kafka_servers, kafka_topic):
+    """Listen for exchange ask/bid updates and publishes them to kafka"""
 
-client = Client(secrets.api_key, secrets.api_secret)
+    client = Client(secrets.api_key, secrets.api_secret)
 
-producer = kafka.KafkaProducer(bootstrap_servers=kafka_servers,
-                         value_serializer=lambda x: 
-                         json.dumps(x).encode('utf-8'))
+    producer = kafka.KafkaProducer(bootstrap_servers=kafka_servers,
+                            value_serializer=lambda x: 
+                            json.dumps(x).encode('utf-8'))
 
-def process_message(msg):
-    msg["timestamp"] = time.time()
-    print(json.dumps(msg))
-    producer.send('binance-BTCUSDT', value=msg)
-    # do something
+    def process_message(msg):
+        msg["timestamp"] = time.time()
+        if verbose:
+            print(json.dumps(msg))
+        producer.send(kafka_topic, value=msg)
+        # do something
 
-bm = BinanceSocketManager(client)
-# start any sockets here, i.e a trade socket
-conn_key = bm.start_symbol_book_ticker_socket('BTCUSDT', process_message)
-# then start the socket manager
-bm.start()
+    bm = BinanceSocketManager(client)
+    # start any sockets here, i.e a trade socket
+    conn_key = bm.start_symbol_book_ticker_socket(symbol, process_message)
+    # then start the socket manager
+    bm.start()
+
+if __name__ == '__main__':
+    go()
--- tweepy/tweets_producer.py
+++ tweepy/tweets_producer.py
@@ -3,23 +3,34 @@ import tweepy
 import kafka
 import json
 import secrets
+import click
 
-kafka_servers = ['localhost:9092']
+@click.command()
+@click.option('-v', '--verbose', default=False, is_flag=True, help='Verbose mode')
+@click.option('-f', '--filter', 'tweets_filter', multiple=True, help='Filter tweets. You can add more than one', default=["bitcoin"])
+@click.option('-k', '--kafka', 'kafka_servers', multiple=True, help='Kafka server. You can add more than one', default=["localhost:9092"])
+@click.option('-t', '--topic', 'kafka_topic', help='Kafka topic in which to publish', default='tweets-bitcoin')
+def go(verbose, tweets_filter, kafka_servers, kafka_topic):
+    """Listen for tweets and publishes them to kafka"""
 
-auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
-auth.set_access_token(secrets.access_token, secrets.access_secret)
+    auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+    auth.set_access_token(secrets.access_token, secrets.access_secret)
 
-producer = kafka.KafkaProducer(bootstrap_servers=kafka_servers,
-                         value_serializer=lambda x: 
-                         json.dumps(x).encode('utf-8'))
+    producer = kafka.KafkaProducer(bootstrap_servers=kafka_servers,
+                            value_serializer=lambda x: 
+                            json.dumps(x).encode('utf-8'))
 
-class MyStreamListener(tweepy.StreamListener):
+    class TweetsStreamListener(tweepy.StreamListener):
 
-    def on_status(self, tweet):
-        print(json.dumps(tweet._json))
-        producer.send('tweets-bitcoin', value=tweet._json)
+        def on_status(self, tweet):
+            if verbose:
+                print(json.dumps(tweet._json))
+            producer.send(kafka_topic, value=tweet._json)
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = auth, listener=myStreamListener)
+    tweetsStreamListener = TweetsStreamListener()
+    twStream = tweepy.Stream(auth = auth, listener=tweetsStreamListener)
 
-myStream.filter(track=['bitcoin'])
+    twStream.filter(track=tweets_filter)
+
+if __name__ == '__main__':
+    go()
