a65d7422bab47c8c88728a5f8381abdeb7ba8ebe
rpo19 <pozzir00@gmail.com>
2020-09-04 21:08:52 +0200

    spark submit executor memory

--- streamapp/submit.sh
+++ streamapp/submit.sh
@@ -5,4 +5,5 @@ export SPARK_HOME=/spark
 exec /spark/bin/spark-class \
 org.apache.spark.deploy.SparkSubmit \
 --master $SPARK_MASTER \
+--executor-memory $SPARK_EXECUTOR_MEMORY \
 /app/target/scala-2.12/StreamApp-assembly-1.0.jar $@
